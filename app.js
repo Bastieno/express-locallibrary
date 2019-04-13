b9439ca7fa2054f558533c1e9a20dda5bb86a535
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-04-13 06:43:02 +0100

    Adding helmet and compression libraries

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var compression = require('compression');
+var helmet = require('helmet');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,6 +10,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+
 
 var app = express();
 //Set up mongoose connection
@@ -21,6 +25,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+app.use(compression()); //Compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
